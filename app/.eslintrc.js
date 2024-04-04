8be344fbfa94856c2421eff1a3d2aac94c1108ec
k3srpn <k3srpn@gmail.com>
2024-04-04 10:00:00 +0900

    Setup react navigator

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    'react/react-in-jsx-scope': 0,
+  },
 };
